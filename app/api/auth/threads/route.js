b7c7a3ad0dee8967c8fd44c2759d84d1cdc7da69
brightalobo-netizen <brightalobo@gmail.com>
2026-08-03 13:38:20 +0100

    Update route.js

--- app/api/auth/threads/route.js
+++ app/api/auth/threads/route.js
@@ -1,6 +1,6 @@
 export async function GET() {
-  const clientId = "1012835358412033";
+  const clientId = "1066588649650087";
   const redirectUri = "https://postthreader.vercel.app/api/auth/callback/threads";
-  const url = `https://www.threads.net/oauth/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=threads_basic,threads_content_publish&response_type=code`;
+  const url = "https://www.threads.net/oauth/authorize?client_id=" + clientId + "&redirect_uri=" + encodeURIComponent(redirectUri) + "&scope=threads_basic,threads_content_publish&response_type=code";
   return Response.redirect(url);
 }
